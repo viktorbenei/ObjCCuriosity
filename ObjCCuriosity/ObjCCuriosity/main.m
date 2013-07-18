baf8c9e8a41ad9a66610634b4f6806386f2b26c2
Viktor Benei <viktor.benei@gmail.com>
2013-07-18 18:46:15 +0200

    basic Mac project, with gitignore

---
+++ ObjCCuriosity/ObjCCuriosity/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ObjCCuriosity
+//
+//  Created by Viktor Benei on 7/18/13.
+//  Copyright (c) 2013 Viktor Benei. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
